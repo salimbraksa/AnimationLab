26e90748a7dc8fec939e432cb29cb027024aa275
salimbraksa <salim.braksa@gmail.com>
2015-12-13 16:54:56 +0000

    First Commit

--- AnimationLab/ViewController.swift
+++ AnimationLab/ViewController.swift
@@ -10,16 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var testView: UIView!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
+      
+      testView.layer.cornerRadius = 3
+
+      
+   }
+   
+   override func animationDidStart(anim: CAAnimation) {
+      
+      let a = (anim as! CABasicAnimation).fromValue
+      let b = (anim as! CABasicAnimation).toValue
+      
+      print("Move from \(a) to \(b) starts at: \((testView.layer.presentationLayer() as! CALayer).position.x)")
+      
    }
 
-   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
+   @IBAction func animate() {
+      
+      // Declare Source / Destination Point
+      let a: CGFloat = CGFloat(random() % 300)
+      let b: CGFloat = CGFloat(random() % 300)
+      
+      // Describe animation
+      let animation = CABasicAnimation(keyPath: "position.x")
+      animation.delegate = self
+      animation.fromValue = a
+      testView.layer.position.x = b       // Maybe I should disable implicit animations ?
+      animation.toValue = b
+      animation.duration = 3
+//      animation.fillMode = kCAFillModeBackwards
+//      animation.removedOnCompletion = false
+      
+//      let pausedTime = testView.layer.convertTime(CACurrentMediaTime(), fromLayer: nil)
+      testView.layer.speed = 0
+//      print("Paused Time: \(pausedTime)")
+      
+      // Add
+      testView.layer.addAnimation(animation, forKey: "Animation")
+      
+      testView.layer.timeOffset = 0 // pausedTime
+      
    }
 
-
+   @IBAction func slide(sender: UISlider) {
+      
+    testView.layer.timeOffset = Double(sender.value) * 3
+      
+   }
+   
 }
 
